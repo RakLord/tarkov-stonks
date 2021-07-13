4acf031810f98c956e54603a04ba2f791ac3db5a
Shaeca16 <s@hotmail.co.uk>
2021-07-13 09:55:04 +0100

    update

--- cogs/administration.py
+++ cogs/administration.py
@@ -3,7 +3,7 @@ from discord.ext import commands
 from utils import log
 
 
-class AdministrationCog(commands.Cog):
+class Administration(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
